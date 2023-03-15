1174b140052ad2d26790afda384439fbaf35b0f0
MikaelVital <61027363+MikaelVital@users.noreply.github.com>
2023-03-15 09:59:02 -0300

    1 file modified

--- src/flow/flow.component.ts
+++ src/flow/flow.component.ts
@@ -10,10 +10,28 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./flow.component.css'],
 })
 export class FlowComponent implements OnInit {
+  numbers: number[] = [];
+  evenNumbers: number[] = [];
+  oddNumbers: number[] = [];
+  numRandom: number;
+
+  constructor() {}
+
+  ngOnInit() {}
+
   getNumbers() {
+    this.numbers = [];
+    for (let i = 0; i < this.numRandom; i++) {
+      this.numbers.push(Math.floor(Math.random() * 100));
+    }
+    this.evenNumbers = this.numbers.filter((num) => num % 2 === 0);
+    this.oddNumbers = this.numbers.filter((num) => num % 2 !== 0);
+  }
+
+  /*   getNumbers() {
     return [10, 6, 23, 97, 1, 27];
   }
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {} */
 }
